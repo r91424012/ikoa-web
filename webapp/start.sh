58081a53539d66ad505e9a2d1654a0048a586605
r91424012 <r91424012@gmail.com>
2020-09-01 09:52:11 +0800

    Update start.sh

--- webapp/start.sh
+++ webapp/start.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
 
-if [[ -n $TEAM_DRIVE_ID && -n $RCLONE_DESTINATION && -n $LOG_PATH && -n $SA_JSON_1 && -n $SA_JSON_2 && -n $SA_JSON_3 && -n $SA_JSON_4 && -n $SA_JSON_5 && -n $SA_JSON_6 && -n $SERIAL_CODE && -n $MERGE_BOOL ]]; then
+if [[ -n $TEAM_DRIVE_ID && -n $RCLONE_DESTINATION && -n $LOG_PATH && -n $SA_JSON_1 && -n $SA_JSON_2 && -n $SERIAL_CODE && -n $MERGE_BOOL ]]; then
     sed -i "/^serial/c\serial = \'$SERIAL_CODE\'" /app/fanza/config.toml   
     echo "team_drive = $TEAM_DRIVE_ID" >> /app/fanza/rclone_1.conf
     echo "team_drive = $TEAM_DRIVE_ID" >> /app/fanza/rclone_2.conf
-    echo "team_drive = $TEAM_DRIVE_ID" >> /app/fanza/rclone_3.conf
-    echo "team_drive = $TEAM_DRIVE_ID" >> /app/fanza/rclone_4.conf
-    echo "team_drive = $TEAM_DRIVE_ID" >> /app/fanza/rclone_5.conf
-    echo "team_drive = $TEAM_DRIVE_ID" >> /app/fanza/rclone_6.conf
     echo "$SA_JSON_1" > /app/fanza/service_account_1.json
     echo "$SA_JSON_2" > /app/fanza/service_account_2.json
-    echo "$SA_JSON_3" > /app/fanza/service_account_3.json
-    echo "$SA_JSON_4" > /app/fanza/service_account_4.json
-    echo "$SA_JSON_5" > /app/fanza/service_account_5.json
-    echo "$SA_JSON_6" > /app/fanza/service_account_6.json
     if [[ $MERGE_BOOL == "false" ]]; then
         sed -i -e "/^merge/c\merge = false" -e "/^m3u_merge/c\m3u_merge = false" /app/fanza/config.toml
     fi
